3ad294e30e70b0804666490910cb55605af8c1a3
Harmony <harmonymukolwe@gmail.com>
2024-07-12 00:01:56 +0300

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+const formData = { dogName: "Byrone", dogBreed: "Poodle" };
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+  .then((response) => response.json())
+  .then((object) => console.log(object))
+  .catch((error) => {
+    alert("Bad things!");
+    console.log(error.message);
+  });
+
+//Lab
+function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json", Accept: "application/json" },
+    body: JSON.stringify({ name: userName, email: userEmail }),
+  })
+    .then((res) => res.json())
+    .then((user) => {
+      console.log(user.id);
+      const p = document.createElement("p");
+      p.textContent = user.id;
+      document.querySelector("body").append(p);
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      const p = document.createElement("p");
+      p.textContent = errorMessage;
+      document.querySelector("body").append(p);
+    });
+}
+
+submitData("Harmony", "harmonymukolwe@gmail.com");
